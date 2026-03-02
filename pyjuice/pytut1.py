e62d5bc116ace79580751ab33994f3bf67714a54
Anna Zhou <Zhoua4573@outlook.com>
2026-03-02 13:12:57 -0500

    Index Practice

---
+++ pyjuice/pytut1.py
@@ -0,0 +1,6 @@
+numberlist = '''0123456789''' #can use ''' for bigger strings
+another = numberlist [:] # print the whole string 
+print (numberlist[1:3]) #prints from 1-4
+print (numberlist[1:-1]) # prints from index 1 to total index (n) -1
+print(numberlist[0])
+print (another)
